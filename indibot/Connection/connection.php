a703a209ba810df8ef92cdc5a1c9e88efffc86a6
Muhammad Aditya <aditya210muhammad@gmail.com>
2018-10-26 09:22:57 +0800

    separating files

---
+++ indibot/Connection/connection.php
@@ -0,0 +1,11 @@
+<?php
+
+    $database = new medoo([
+        'database_type' => 'mysql',
+        'database_name' => 'indi_banua',
+        'server' => 'localhost',
+        'username' => 'root',
+        'password' => 'r00t',
+        'charset' => 'utf8'
+    ]);
+?>
--- indibot/database.php
+++ indibot/database.php
@@ -1,6 +1,12 @@
 <?php
 require_once 'medoo.php';
-require_once 'connection.php';
+require_once 'Connection/connection.php';
+
+if ($database == true) {
+    echo "true";
+} else {
+    echo "false";
+}
 
 function insert_order($id_list_order,$order_client,$status_order,$keterangan_client)
 {
